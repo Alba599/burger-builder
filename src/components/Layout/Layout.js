f432b1799154f4eeb5e2273249322aab2f92c39f
Alba599 <57162043+Alba599@users.noreply.github.com>
2020-10-25 10:00:11 -0500

    creation of files

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+// const { propTypes } = require("react-bootstrap/esm/Image");
+
+import React from 'react';
+import Aux from '../../hoc/Auxiliary'
+
+const layout = (props) => (
+    <Aux>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main>{props.children}</main>
+    </Aux>
+);
+
+export default layout;
